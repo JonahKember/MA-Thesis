3b558f3071b5aecdac107e721fdfd68ad9ad69ad
JonahKember <81769550+JonahKember@users.noreply.github.com>
2021-04-20 15:48:59 -0400

    Update dynamicCommStruc.m

--- dynamicCommStruc.m
+++ dynamicCommStruc.m
@@ -10,30 +10,38 @@ function dynamicCommStruc = dynamicCommStruc(staticCommStruc, minJaccard)
 %  
 %   After assessing the similarity between each community at (t) with each community at (t + 1), reassign each community at (t + 1) as the community with which it is most 
 %   similar. If two communities fail to meet the minimum similarity value ('minJaccard'), assign them random values. If two communities at (t + 1) share a community with which
-%   they are most similar, they will be considered the same community (Rugerro & Bettinardi, 2021; Cherifi et al., 2019).
+%   they are most similar, they will be considered the same community.
 %   
 %   Jaccard-Index: Size of the intersection of two sets divided by the size of the union of two sets.
 % 
 %  INPUT:
 %               
 %            staticCommStruc      =    [Node X Time] matrix with the community affiliation of each node for every time point (detected from static adjacency matrices).
-%            minJaccard            =    Minimum similarity required to be considered the same community (0 to 1).    
+%            minJaccard            =   Minimum similarity required to be considered the same community (0 to 1).    
 %               
 %  OUTPUT:           
 %
 %            dynamicCommStruc    =    [Node X Time] matrix with the community affiliation of each node for every time point. 
-%                                     Communities at (t) correspond to the communities with which they were most similar to at (t-1).
+%                                     Communities at (t) correspond to the communities with which they were most similar to at (t - 1).
 %
 %Reference:
 % 
 % Cherifi, H., Szymanski, B.K. et al. On community structure in complex networks: challenges and opportunities.
 %       Appl Netw Sci 4, 117 (2019). https://doi.org/10.1007/s41109-019-0238-9
 %
-%%
+%% Assign Each Community a Unique Value
+ 
+time = size(staticCommStruc,2);                             
+r = rand(time,1);                                           
+
+for n = 1:time
+    staticCommStruc(:,n) = staticCommStruc(:,n) + r(n);
+end
 
 for timeOne = 1:size(staticCommStruc,2)-1
-    
-timeTwo = timeOne + 1;
+    timeTwo = timeOne + 1;
+
+%% Create a Matrix for each Community at time point (t) and (t + 1), then measure the similarity between each pair of communities
 
 A_vector = staticCommStruc(:,timeOne);
 B_vector = staticCommStruc(:,timeTwo);
@@ -42,7 +50,7 @@ A_mat = zeros(length(A_vector));
 B_mat = zeros(length(B_vector));
 
 for n = 1:length(A_vector)
-    for nn = 1:length(A_vector)       
+    for nn = 1:length(A_vector)                             
         if A_vector(n) == A_vector(nn)
             A_mat(n,nn) = A_vector(n);
          end
@@ -57,22 +65,28 @@ for n = 1:length(B_vector)
     end
 end
 
-A_mat = A_mat - diag(diag(A_mat));
-B_mat = B_mat - diag(diag(B_mat));
+A_mat(find(A_mat == 0)) = max(max(A_mat))+1;
+B_mat(find(B_mat == 0)) = max(max(B_mat))+1;
 
-commStrucSim = [];
+commStrucSim = [];  
 
-for n = 1:length(unique(A_mat)) 
+uniqueANZ = unique(A_mat);
+if length(uniqueANZ) ~= 1       
+uniqueANZ = uniqueANZ(uniqueANZ ~= max(max(A_mat)));
+end
+uniqueBNZ = unique(B_mat);
+uniqueBNZ = uniqueBNZ(uniqueBNZ ~= max(max(B_mat)));
+
+for n = 1:length(uniqueANZ)
     numa = unique(A_mat);
-    A_bin = A_mat == numa(n);
-    
+    A_bin = A_mat == numa(n);                       
     j = [];
     J =[];
-    
-    for nn = 1:length(unique(B_mat))
+   
+    for nn = 1:length(uniqueBNZ)
         numb = unique(B_mat);
         B_bin = B_mat == numb(nn);
-   
+       
         for kk = 1:127
             a_diag = diag(A_bin,kk);
             b_diag = diag(B_bin,kk);
@@ -82,28 +96,36 @@ for n = 1:length(unique(A_mat))
             if isnan(jac)  
                jac = 0;
             end
-            J = [J,jac];
-        end        
+            J = [J,jac]; 
+        end
     J = mean(J);
     j = [j,J];
     end
-commStrucSim = [commStrucSim;j];
+    commStrucSim = [commStrucSim;j];  % 'commStrucSim' is a matrix that shows the similarity between every possible pair of communities.
+end
+    if size(commStrucSim,1) == 1 
+    commStrucSim(2,:) = commStrucSim;
 end
 
-        uni_b = unique(B_mat);
-        [simOfNeighbour,locationSimNeighbour] = max(commStrucSim);
+%% For each community in time point (t + 1), reassign it the value of the community with which it is most similar to in time (t)
 
-for nn = 1:length(uni_b)
-    changeFrom = uni_b(nn);   
-            if simOfNeighbour(nn) > minJaccard    
-               changeTo = locationSimNeighbour(nn);
+[simOfNeighbour,locationSimNeighbour] = max(commStrucSim); %locationSimNeighbour is a vector telling which communities in timeB (columns) correspond to which communities in timeA (rows)
+       
+        numa = unique(staticCommStruc(:,timeOne));
+        numb = unique(staticCommStruc(:,timeTwo));
+        
+for nn = 1:length(simOfNeighbour)
+    if simOfNeighbour(nn) > minJaccard  
+        changeFrom = numb(nn);
+               changeTo = numa(locationSimNeighbour(nn));          
                staticCommStruc(:,timeTwo) = changem(staticCommStruc(:,timeTwo),changeTo,changeFrom);
-            else          
+    else            
+               changeFrom = numb(nn); 
                changeTo = rand(1);
-               staticCommStruc(:,timeTwo) = changem(staticCommStruc(:,timeTwo),changeTo,changeFrom);
-            end                    
+               staticCommStruc(:,timeTwo) = changem(staticCommStruc(:,timeTwo),changeTo,changeFrom);           
+    end
 end
 
 dynamicCommStruc = staticCommStruc;
-
+end
 end
